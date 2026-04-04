0ed7fd1ea6363e9294d04eed5a1fb66e3cf60a9f
parthasahap987-spec <parthasahap987@gmail.com>
2026-04-04 18:08:19 +0530

    Update auto_post.py

--- auto_post.py
+++ auto_post.py
@@ -1,4 +1,3 @@
-
 import re
 import requests
 import io
@@ -6,7 +5,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
 # 🔑 CONFIG
-BOT_TOKEN = "8645119625:AAEnBkJ5ND1z06BS9ui4YX_nkFJ8kwLgFY0"
+BOT_TOKEN = "8645119625:AAHBcJMUee_d3s6rNRzhTZ2bNFLGyhcl_sI"
 CHANNELS = [-1002161382456]
 AFFILIATE_TAG = "partha07e-21"
 BITLY_TOKEN = "e3df1684c678e66ab90b1a3746f57852e4b3eff0"
